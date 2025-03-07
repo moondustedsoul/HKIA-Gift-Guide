0eb06642eafdd27ce177630785c648a1b58ee2bd
Baylee Preston <brprest1@asu.edu>
2025-03-07 16:23:26 -0700

    sort terms by longest first

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ async function loadTermImages() {
 
 // Replace terms with images in text
 function replaceTermsWithImages(text) {
-    Object.keys(termImages).forEach(term => {
+    // Sort terms by length (longest first)
+    const sortedTerms = Object.keys(termImages).sort((a, b) => b.length - a.length);
+
+    // Replace terms with images
+    sortedTerms.forEach(term => {
         const regex = new RegExp(`\\b${term}\\b`, "gi");
         const imageTag = `<img src="${termImages[term]}" alt="${term}" style="width: 20px; vertical-align: middle;">`;
-        text = text.replace(regex, `${imageTag} ${term}`);
+
+        // Replace the term with its corresponding image
+        text = text.replace(regex, imageTag + " " + term);
     });
+
     return text;
 }
 
